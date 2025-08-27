4c0adca4a44d09ce3692907581fca58b36e5d3fd
wangtengfei <wtf1943@gmail.com>
2025-08-27 18:26:44 +0800

    add evn

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from exchange.okx import OKXExchange
 from indicators import Strength, TechnicalIndicators
 import schedule
@@ -6,7 +7,10 @@ import time
 
 okx_client = OKXExchange()
 indicators = TechnicalIndicators()
-
+leverage = os.getenv("LEVERAGE", "5")
+grid_leverage = os.getenv("GRID_LEVERAGE", "5")
+amount = os.getenv("AMOUNT", 200)
+grid_amount = os.getenv("GRID_AMOUNT", "300")
 def main():
     start_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     print(f'开始执行策略：{start_time}')
@@ -21,18 +25,18 @@ def main():
     print(f'trend:{signal.trend},strength:{signal.strength}')
     # 持仓逻辑
     if signal.trend == 1:
-        okx_client.open_position(symbol=symbol,direction='long',amount=200,leverage=5)
+        okx_client.open_position(symbol=symbol,direction='long',amount=amount,leverage=leverage)
         # 开多网格
-        okx_client.open_grid_if_not_exist(symbol=symbol,direction='long',amount=300,leverage=5,atr=atr_value)
+        okx_client.open_grid_if_not_exist(symbol=symbol,direction='long',amount=grid_amount,leverage=grid_leverage,atr=atr_value)
     elif signal.trend == -1:
-        okx_client.open_position(symbol=symbol,direction='short',amount=200,leverage=5)
+        okx_client.open_position(symbol=symbol,direction='short',amount=amount,leverage=leverage)
         # 开多网格
-        okx_client.open_grid_if_not_exist(symbol=symbol,direction='short',amount=300,leverage=5,atr=atr_value)
+        okx_client.open_grid_if_not_exist(symbol=symbol,direction='short',amount=grid_amount,leverage=grid_leverage,atr=atr_value)
 
     if signal.strength == Strength.WEAK:
         # 检查如果 多单网格 或者 空单网格不存在，就开启
-        okx_client.open_grid_if_not_exist(symbol=symbol,direction='short',amount=300,leverage=5,atr=atr_value)
-        okx_client.open_grid_if_not_exist(symbol=symbol,direction='long',amount=300,leverage=5,atr=atr_value)
+        okx_client.open_grid_if_not_exist(symbol=symbol,direction='short',amount=grid_amount,leverage=grid_leverage,atr=atr_value)
+        okx_client.open_grid_if_not_exist(symbol=symbol,direction='long',amount=grid_amount,leverage=grid_leverage,atr=atr_value)
         pass
     elif signal.trend == 1:
         # 关闭做空网
